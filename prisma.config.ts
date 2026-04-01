6dff67090818f4d3e1b2c1c9794ada7a931c4874
Harsha Vardhan <notharsha.007@gmail.com>
2026-04-01 12:42:16 +0530

    small change in location of datasource

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,4 +6,7 @@ export default defineConfig({
   migrations: {
     path: "prisma/migrations",
   },
+  datasource: {
+    url: process.env.DIRECT_DATABASE_URL ?? process.env.DATABASE_URL ?? "",
+  },
 });
